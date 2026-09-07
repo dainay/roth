497c78686d7a9ed1a73dfdbe70e9d78dabb5dcc3
dainay <iarovaiadaria@gmail.com>
2026-09-07 16:07:21 +0200

    Fixed env

--- vite.config.js
+++ vite.config.js
@@ -32,56 +32,56 @@ export default defineConfig(({ mode }) => {
             // },
         },
 
-        server: {
-            proxy: {
-                '/api': {
-                    target: backendTarget,
-                    changeOrigin: true,
-                    secure: false,
-                    // headers: {
-                    //     Host: 'testwww.roth-france.fr',
-                    // },
+        // server: {
+        //     proxy: {
+        //         '/api': {
+        //             target: backendTarget,
+        //             changeOrigin: true,
+        //             secure: false,
+        //             // headers: {
+        //             //     Host: 'testwww.roth-france.fr',
+        //             // },
 
-                    configure(proxy) {
-                        proxy.on('proxyReq', (proxyReq, req) => {
-                            console.log(
-                                `[api proxy] ${req.method} ${req.url} -> ${proxyReq.path}`
-                            )
-                        })
+        //             configure(proxy) {
+        //                 proxy.on('proxyReq', (proxyReq, req) => {
+        //                     console.log(
+        //                         `[api proxy] ${req.method} ${req.url} -> ${proxyReq.path}`
+        //                     )
+        //                 })
 
-                        proxy.on('proxyRes', (proxyRes, req) => {
-                            console.log(
-                                `[api proxy] ${req.method} ${req.url} <- ${proxyRes.statusCode}`
-                            )
-                        })
+        //                 proxy.on('proxyRes', (proxyRes, req) => {
+        //                     console.log(
+        //                         `[api proxy] ${req.method} ${req.url} <- ${proxyRes.statusCode}`
+        //                     )
+        //                 })
 
-                        proxy.on('error', (error, req) => {
-                            console.error(
-                                `[api proxy error] ${req.method} ${req.url}`,
-                                error
-                            )
-                        })
-                    },
-                },
+        //                 proxy.on('error', (error, req) => {
+        //                     console.error(
+        //                         `[api proxy error] ${req.method} ${req.url}`,
+        //                         error
+        //                     )
+        //                 })
+        //             },
+        //         },
 
-                '/photos': {
-                    target: backendTarget,
-                    changeOrigin: true,
-                    secure: false,
-                    // headers: {
-                    //     Host: 'testwww.roth-france.fr',
-                    // },
-                },
-                  '/catalogue': {
-                    target: backendTarget,
-                     changeOrigin: true,
-                    secure: false,
-                    cookieDomainRewrite: '',
-                    //  headers: {
-                    //     Host: 'testwww.roth-france.fr',
-                    // },
-                },
-            },
-        },
+        //         '/photos': {
+        //             target: backendTarget,
+        //             changeOrigin: true,
+        //             secure: false,
+        //             // headers: {
+        //             //     Host: 'testwww.roth-france.fr',
+        //             // },
+        //         },
+        //           '/catalogue': {
+        //             target: backendTarget,
+        //              changeOrigin: true,
+        //             secure: false,
+        //             cookieDomainRewrite: '',
+        //             //  headers: {
+        //             //     Host: 'testwww.roth-france.fr',
+        //             // },
+        //         },
+        //     },
+        // },
     }
 })
